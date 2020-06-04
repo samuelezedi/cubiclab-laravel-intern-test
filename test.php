19a9da10bad538ef8463ac2a99d7a4480c348ca6
Samuel Ezedi <samuelezedi@gmail.com>
2020-06-04 20:51:21 +0100

    initial commit

---
+++ test.php
@@ -0,0 +1,31 @@
+<?php
+
+// Complete the function below, The function is a recursive function
+// that print all even numbers from 1 to 100.
+
+function recurse($num) {
+	//complete the function, write your code inside here
+}
+
+recurse(100);
+
+
+// What will be the result of this laravel route command below,
+// Share your conclusion
+
+Route('login/24', function($id){
+	
+	echo $id;
+	
+});
+
+?>
+
+<!-- Write a simple javascript function to change the color of paragraph when user clicks the "Change Color" button. -->
+
+<div>
+	<p id="paragraph" style="color:pink" >This is just a test chill out.</p>
+	<button>Change Color</button>
+</div>
+
+
